6c448407c5becdffe7b141ca44e8fa3145860a2f
Thanh Truyen <truyennguyends@gmail.com>
2017-05-02 17:47:42 +0700

    phân loại method tìm thấy

---
+++ tinhtruyen/ClassifySensitiveMethod.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+class MethodClassifed:
+    method = ''
+    type = ''
+
+def ClassifySensitiveMethod(ListMethod):
+    sink = []
+    source = []
+    filesink = open("./Ouput_CatSinks.txt", "r")
+    filesource = open("./Ouput_CatSources.txt", "r")
+    tmp = MethodClassifed()
+    for method in ListMethod:
+        for line in filesink:
+            if line.endswith(":\n"):
+                type = line[:-2]
+            elif method == line[:-1]:
+                tmp.method = method
+                tmp.type = type
+                sink.append(tmp)
+        for line in filesource:
+            if line.endswith(":\n"):
+                type = line[:-2]
+            elif method == line[:-1]:
+                tmp.method = method
+                tmp.type = type
+                source.append(tmp)
+    return sink, source
