32f214265f1d9e5e02b1aa0750f9f53a24d7ef4a
Tinh Huynh <gm4drvpn01@gmail.com>
2017-05-07 10:22:37 +0700

    update tinhtruyen

--- tinhtruyen/tinhtruyen.py
+++ tinhtruyen/tinhtruyen.py
@@ -25,3 +25,35 @@ def get_method_invoke():
                     tmp.submethods.append(get_endsplitstring(line))
         k.append(tmp)
     return k
+
+
+def ClassifySensitiveMethod(ListMethod):
+    smali = 'smali.'
+    filesink = open('tinhtruyen/Ouput_CatSinks.txt', "r")
+    filesource = open('tinhtruyen/Ouput_CatSources.txt', "r")
+    ListMethodClassified = []
+    for Item in ListMethod:
+        tmp = MethodItem()
+        tmp.nmethod = Item.nmethod[Item.nmethod.index(smali) + len(smali):-7]
+        for submethod in Item.submethods:
+            for line in filesink:
+                if line.endswith(":\n"):
+                    type = line[:-2]
+                elif submethod == line[:-1]:
+                    strtmp=submethod + "#sink" + "@" + type
+                    tmp.submethods.append(strtmp)
+                    break
+            filesink.seek(0,0)
+            for line in filesource:
+                if line.endswith(":\n"):
+                    type = line[:-2]
+                elif submethod == line[:-1]:
+                    strtmp = submethod + "#source" + "@" + type
+                    tmp.submethods.append(strtmp)
+                    break
+            filesource.seek(0,0)
+        ListMethodClassified.append(tmp)
+    return ListMethodClassified
+k = get_method_invoke()
+L = ClassifySensitiveMethod(k)
+t = "1"
